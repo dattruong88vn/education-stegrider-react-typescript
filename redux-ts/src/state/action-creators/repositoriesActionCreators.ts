1e8555a3307c6fd71f9b1bb3038b5a5f6ae24b25
dat.truong <dat.truong@secomm.vn>
2024-02-22 19:06:30 +0700

    video 40: setup redux and export

---
+++ redux-ts/src/state/action-creators/repositoriesActionCreators.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { Dispatch } from "redux";
+
+import { SearchRepositoriesActionTypes } from "../action-types";
+import { SearchRepositoriesActions } from "../actions";
+
+export const searchRepositories = (term: string) => {
+  return async (dispatch: Dispatch<SearchRepositoriesActions>) => {
+    dispatch({
+      type: SearchRepositoriesActionTypes.SEARCH_REPOSITORIES,
+    });
+    try {
+      const { data } = await axios.get(
+        `https://registry.npmjs.org/-/v1/search?text=${term}`
+      );
+      const result = data.object.map((result: any) => result.package.name);
+      dispatch({
+        type: SearchRepositoriesActionTypes.SEARCH_REPOSITORIES_SUCCESS,
+        payload: result,
+      });
+    } catch (err) {
+      if (err instanceof Error) {
+        dispatch({
+          type: SearchRepositoriesActionTypes.SEARCH_REPOSITORIES_FAILED,
+          payload: err.message,
+        });
+      }
+    }
+  };
+};
