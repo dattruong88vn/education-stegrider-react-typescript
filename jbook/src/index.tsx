dbe1a5b7595e84cb5cd3619e497318d612c19531
dat.truong <dat.truong@secomm.vn>
2024-02-29 16:33:01 +0700

    212: action and reducer

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,13 +1,18 @@
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+
 import "../src/styles/base.css";
 import TextEditor from "./components/textEditor/TextEditor";
+import store from "./state/store";
 
 const App = () => {
   return (
-    <div>
-      {/* <CodeCell /> */}
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        {/* <CodeCell /> */}
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
