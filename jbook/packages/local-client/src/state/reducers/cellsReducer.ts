31528cf9263646e49e2cd8e2920a5985a31fe86b
dat.truong <dat.truong@secomm.vn>
2024-03-06 08:53:25 +0700

    318: fetchCell and saveCell actions

--- jbook/packages/local-client/src/state/reducers/cellsReducer.ts
+++ jbook/packages/local-client/src/state/reducers/cellsReducer.ts
@@ -30,21 +30,23 @@ const initialState = {
 
 const reducer = produce((state: CellsState = initialState, action: Action) => {
   switch (action.type) {
-    case ActionType.UPDATE_CELL:
+    case ActionType.UPDATE_CELL: {
       const { id, content } = action.payload;
 
       state.data[id].content = content;
       return state;
+    }
 
-    case ActionType.DELETE_CELL:
+    case ActionType.DELETE_CELL: {
       delete state.data[action.payload];
       // delete in data
       const indexDelete = state.order.findIndex((id) => id === action.payload);
       // delete in order
       if (indexDelete !== -1) state.order.splice(indexDelete, 1);
       return state;
+    }
 
-    case ActionType.MOVE_CELL:
+    case ActionType.MOVE_CELL: {
       const { direction } = action.payload;
       const index = state.order.findIndex((id) => id === action.payload.id);
       const targetIndex = direction === "up" ? index - 1 : index + 1;
@@ -57,7 +59,9 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
       state.order[targetIndex] = action.payload.id;
 
       return state;
-    case ActionType.INSERT_CELL_AFTER:
+    }
+
+    case ActionType.INSERT_CELL_AFTER: {
       const cell: Cell = {
         content: "",
         id: randomId(),
@@ -76,6 +80,38 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
         state.order.splice(foundIndex + 1, 0, cell.id);
       }
       return state;
+    }
+
+    case ActionType.FETCH_CELLS: {
+      state.loading = true;
+      state.error = null;
+      return state;
+    }
+
+    case ActionType.FETCH_CELLS_COMPLETE: {
+      state.loading = false;
+      state.error = null;
+      state.order = action.payload.map((cell) => cell.id);
+
+      action.payload.reduce((acc, cell) => {
+        acc[cell.id] = cell;
+        return acc;
+      }, {} as CellsState["data"]);
+
+      return state;
+    }
+
+    case ActionType.FETCH_CELLS_ERROR: {
+      state.loading = false;
+      state.error = action.payload;
+      return state;
+    }
+
+    case ActionType.SAVE_CELLS_ERROR: {
+      state.loading = false;
+      state.error = action.payload;
+      return state;
+    }
 
     default:
       return state;
